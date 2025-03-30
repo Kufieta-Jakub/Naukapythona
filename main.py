1cd97879c8ea51e1df44178e207f4fd4424d343c
Kufieta-Jakub <kufietajakub@gmail.com>
2025-03-30 22:02:51 +0200

    csv files 1

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-file = open("Plik.txt")
-for line in file:
-    print(line, end=" ")
+import csv
+
+# file = open("Plik.txt")
+# for line in file:
+#     print(line, end=" ")
+# file.close()
+
+file =  open("names_towns.csv","r",encoding="utf-8")
+csv_reader = csv.reader(file)
+names = []
+surnames = []
+towns = []
+next(csv_reader)
+for line in csv_reader:
+    names.append(line[0])
+    surnames.append(line[1])
+    towns.append(line[2])
+
+print(len(names))
+
+for i in range(len(names)):
+    print("Pan/Pani " + names[i] + " " + surnames[i]  +" mieszka w "+ towns[i] )
+file.close()
